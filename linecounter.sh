7e41adff33c00a74f30d349d5e28f6d6f4acf05b
reyoung <reyoung@126.com>
2011-12-05 02:20:49 +0800

    Add linecounter.sh

---
+++ linecounter.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+GlobalCounter=0
+handleWithCorrectFile(){
+	fn=$1
+	line=`cat $fn| wc -l`
+	let "GlobalCounter+=$line"
+}
+
+
+handleFile(){
+	filename=$1
+	ext=${filename##*.}
+	if [ $ext = c -o $ext = h ];then
+		handleWithCorrectFile "$filename"
+	else if [ $ext = ld -o $ext = S ]; then
+		handleWithCorrectFile "$filename" 
+	fi
+	fi
+}
+
+
+handleDir(){
+	dirname=$1
+	files=`ls $1`
+	for fn in $files
+	do
+		if [ -d $1/$fn ];
+		then 
+			handleDir $1/$fn
+		else
+			handleFile $1/$fn
+		fi
+	done
+}
+
+handleDir .
+echo $GlobalCounter
