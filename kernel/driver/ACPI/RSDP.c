dab0fe1e5d2fa0b836e51ab3483093b92414fafa
reyoung <reyoung@126.com>
2011-12-05 12:40:02 +0800

    Refine RSDP.c

--- kernel/driver/ACPI/RSDP.c
+++ kernel/driver/ACPI/RSDP.c
@@ -4,28 +4,31 @@
 #define RSDP_MAGIC_LEN 8
 #define BIOS_MEM_BEGIN ((void*)(0x000E0000))
 #define BIOS_MEM_END   ((void*)(0x000FFFFF))
-
+static void* RSDP_Position = 0;
 
 int RSDP_GetVersion(){
-	char * p = BIOS_MEM_BEGIN;
-	while(p!=BIOS_MEM_END){
-		if(strncmp(p,RSDP_MAGIC,RSDP_MAGIC_LEN)==0){
-			return (int ) p[15]+1;
-		}
-		++p;
+	char* p = (char*)RSDP_GetDescriptor();
+	if(p!=0){
+		return (int ) p[15]+1;
+	} else {
+		return -1;
 	}
-	return -1;
 }
 
 void* RSDP_GetDescriptor(){
-	char * p =BIOS_MEM_BEGIN;
-	while(p!=BIOS_MEM_END){
-		if(strncmp(p,RSDP_MAGIC,RSDP_MAGIC_LEN)==0){
-			return p;
+	if(RSDP_Position!=0){
+		return RSDP_Position;
+	} else {
+		char * p =BIOS_MEM_BEGIN;
+		while(p!=BIOS_MEM_END){
+			if(strncmp(p,RSDP_MAGIC,RSDP_MAGIC_LEN)==0){
+				RSDP_Position = p;
+				return RSDP_Position;
+			}
+			++p;
 		}
-		++p;
+		return 0;
 	}
-	return 0;
 }
 
 boolean RSDP_IsValid(){
